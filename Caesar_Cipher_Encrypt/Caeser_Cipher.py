6d22fbb1e996d4d3416d9e6a697fe893e67ef770
Aditya Kumar <aditya.070427@gmail.com>
2026-01-13 19:48:05 +0530

    your message

--- Caesar_Cipher_Encrypt/Caeser_Cipher.py
+++ Caesar_Cipher_Encrypt/Caeser_Cipher.py
@@ -1,35 +1,7 @@
 import art
 print(art.logo)
-#Encoding the messages.
+
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
-
-# direction = input("Type 'encode' to encrypt or 'decode' to decrypt\n").lower()
-# text = input("Type your message\n")
-# shift = int(input("Type the shift number:\n"))
-
-# def encrypt(original_text, shift_amount):
-#     cipher_text = ""
-#     for letter in original_text:
-#         shifted_position = alphabet.index(letter) + shift_amount
-#
-#         shifted_position %= len(alphabet)
-#         cipher_text += alphabet[shifted_position]
-#     print(f"Here is the Encoded result: {cipher_text}")
-#
-# # encrypt(original_text=text, shift_amount=shift)
-#
-# #Decoding the messages.
-#
-# def decrypt(original_text, shift_amount):
-#     output_text = ""
-#     for letter in original_text:
-#         shifted_position = alphabet.index(letter) - shift_amount
-#
-#         shifted_position %= len(alphabet)
-#         output_text += alphabet[shifted_position]
-# #     print(f"Here is the Encoded result: {output_text}")
-
-
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
@@ -53,7 +25,7 @@ while should_continue:
     text = input("Type your message\n")
     shift = int(input("Type the shift number:\n"))
 
-#decrypt(original_text=text, shift_amount=shift)
+
 
     caesar(original_text=text, shift_amount=shift,encode_or_decode=direction)
     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
