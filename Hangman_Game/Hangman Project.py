2a161035f0a32f1f294789a282fc3ed0ce1fe53a
Aditya Kumar <aditya.070427@gmail.com>
2026-06-09 14:21:01 +0530

    Refactor Hangman game logic and improve code readability

--- Hangman_Game/Hangman Project.py
+++ Hangman_Game/Hangman Project.py
@@ -2,11 +2,7 @@ import random
 from Hangman_ASCII_ARTS import Stages, logo
 from Hangman_words import word_list
 
-
-
-
 chosen_word = random.choice(word_list)
-# print(f" The word is: {chosen_word}")
 
 correct_letters = []
 lives = 6
@@ -15,37 +11,28 @@ game_over = False
 while not game_over:
     guess = input("Guess a letter: ").lower()
 
-    # REPEAT GUESS PROTECTION
     if guess in correct_letters:
         print(f"You already guessed '{guess}'. Try another letter!")
         continue
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Wrong guess! Lives left: {lives}")
 
     correct_letters.append(guess)
 
     display = ""
     for letter in chosen_word:
-        if letter in correct_letters:
-            display += letter
-        else:
-            display += "_"
+        display += letter if letter in correct_letters else "_"
 
     print(display)
-
-    # WRONG guess = lose life
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"Wrong guess! Lives left: {lives}")
-
-    # Print the stage AFTER updating lives
     print(Stages[6 - lives])
 
-    # Lost all lives.
     if lives == 0:
         print("You Lose!")
         print(f"The word was: {chosen_word}")
         game_over = True
 
-    # Guessed whole word
-    if "_" not in display:
+    elif "_" not in display:
         print("You Win!")
         game_over = True
