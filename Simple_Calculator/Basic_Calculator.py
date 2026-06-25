258d083780f3413268e91fb4a8dca4cc6181e754
Aditya Kumar <aditya.070427@gmail.com>
2026-06-25 23:40:58 +0530

    Optimize calculator logic and remove recursive function calls

--- Simple_Calculator/Basic_Calculator.py
+++ Simple_Calculator/Basic_Calculator.py
@@ -1,19 +1,20 @@
 import Calculator_Art
 
-def add(n1,n2):
-    return n1+n2
+
+def add(n1, n2):
+    return n1 + n2
 
 
-def subtract(n1,n2):
-    return n1-n2
+def subtract(n1, n2):
+    return n1 - n2
 
 
-def multiply(n1,n2):
-    return n1*n2
+def multiply(n1, n2):
+    return n1 * n2
 
 
-def divide(n1,n2):
-    return n1/n2
+def divide(n1, n2):
+    return n1 / n2
 
 
 operations = {
@@ -22,27 +23,38 @@ operations = {
     "*": multiply,
     "/": divide,
 }
+
+
 def calculator():
-    print(Calculator_Art.art)
-    should_accumulate = True
-    num_1 = float(input("What is the first number: "))
+    while True:
+        print(Calculator_Art.art)
 
-    while should_accumulate:
+        num_1 = float(input("What is the first number: "))
+        should_accumulate = True
 
-        for symbol in operations:
-            print(symbol)
-        operation_symbol = input("Pick an Operation: ")
-        num_2 = float(input("What is the second number: "))
-        answer = operations[operation_symbol](num_1, num_2)
-        print(f" {num_1}  {operation_symbol}  {num_2} = {answer}")
+        while should_accumulate:
 
-        choice = input(f" Type 'y' to continue calculating with {answer}, or type 'n' to start new calculations : ")
+            for symbol in operations:
+                print(symbol)
 
-        if choice == "y":
-            num_1 = answer
-        else:
-            should_accumulate = False
-            print("\n" * 20)
-            calculator()
+            operation_symbol = input("Pick an operation: ")
+            num_2 = float(input("What is the second number: "))
+
+            calculation_function = operations[operation_symbol]
+            answer = calculation_function(num_1, num_2)
+
+            print(f"{num_1} {operation_symbol} {num_2} = {answer}")
+
+            choice = input(
+                f"Type 'y' to continue calculating with {answer}, "
+                f"or type 'n' to start a new calculation: "
+            ).lower()
+
+            if choice == "y":
+                num_1 = answer
+            else:
+                should_accumulate = False
+                print("\n" * 20)
+
 
 calculator()
